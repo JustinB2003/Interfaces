9e3722829c909ca61f4e0f0ae0f893f3de4796ba
JustinB2003 <163298446+JustinB2003@users.noreply.github.com>
2024-04-23 13:46:38 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public interface IPlayable
+{
+    void Play();
+}
+
+public class Guitar : IPlayable
+{
+    public void Play()
+    {
+        Console.WriteLine("GUITAR IS NOW PLAYING!");
+    }
+}
+
+public class Piano : IPlayable
+{
+    public void Play()
+    {
+        Console.WriteLine("PIANO IS NOW PLAYING!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Guitar guitar = new Guitar();
+        Piano piano = new Piano();
+
+        guitar.Play();
+        piano.Play();
+    }
+}
